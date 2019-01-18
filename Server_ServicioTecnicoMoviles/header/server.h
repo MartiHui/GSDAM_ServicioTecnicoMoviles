be492f94b8b8713557f2b76bcf359559fe6c6c47
MartiHui <martiyanghui@gmail.com>
2019-01-18 14:50:07 +0100

    Me habia olvidado de guardar antes del commit anteior

--- Server_ServicioTecnicoMoviles/header/server.h
+++ Server_ServicioTecnicoMoviles/header/server.h
@@ -2,16 +2,28 @@
 #define SERVER_H
 
 #include <QObject>
+#include <QList>
+
+class QWebSocketServer;
+class QWebSocket;
 
 class Server : public QObject
 {
     Q_OBJECT
 public:
-    explicit Server(QObject *parent = nullptr);
+    explicit Server(quint16 port);
+    ~Server();
+
+private:
+    QWebSocketServer *m_webSocketServer;
+    QList<QWebSocket *> m_clients;
 
 signals:
+    void close();
 
-public slots:
+private slots:
+    void socketConnected();
+    void socketDisconnected();
 };
 
 #endif // SERVER_H
--- Server_ServicioTecnicoMoviles/source/server.cpp
+++ Server_ServicioTecnicoMoviles/source/server.cpp
@@ -1,6 +1,15 @@
+#include <QWebSocketServer>
+#include <QWebSocket>
+#include <QDebug>
+
 #include "server.h"
 
-Server::Server(QObject *parent) : QObject(parent)
-{
+Server::Server(quint16 port) {
+    m_webSocketServer(new QWebSocketServer(QStringLiteral("Servidor Servicio Tecnico de Moviles"),
+                                           QWebSocket::NonSecureMode, this));
+    if (m_webSocketServer->listen(QHostAddress::Any, port)) {
+        qDebug() << "Servidor iniciado. Puerto: " << port;
 
+        connect()
+    }
 }
