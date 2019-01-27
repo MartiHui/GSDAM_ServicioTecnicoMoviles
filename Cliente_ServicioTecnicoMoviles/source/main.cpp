615a1b3acf52d5bad8f6a03ce9a365c0cb7ec953
MartiHui <martiyanghui@gmail.com>
2019-01-27 21:06:26 +0100

    Pruebas: los clientes pueden conectarse al servidor

--- Cliente_ServicioTecnicoMoviles/source/main.cpp
+++ Cliente_ServicioTecnicoMoviles/source/main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QWebSocket>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    //MainWindow w;
+    //w.show();
+    QWebSocket socket;
+    socket.open(QUrl("ws://localhost:1234"));
 
     return a.exec();
 }
